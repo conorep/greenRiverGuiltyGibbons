1aa1ebab1c6d2b25abe55e3b4fe41d694dd105b5
McZazz <91503026+McZazz@users.noreply.github.com>
2021-10-29 20:37:56 -0700

    testing Q form for iframe submit

---
+++ submissionSuccess.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>success</title>
+    </head>
+    <body>
+        <?php
+        // send imap_getmailboxes
+        $toEmail = "Price.Kevin@student.greenriver.edu";
+        $fromName = "fromename"; // $to would be the admin, $from would be a form variable from the submit form
+        $fromEmail = "fromemail"; //this will be fromt eh faq form
+        $subject = "New Order";
+        $headers = "From: $fromName <$fromEmail>";
+        $message = "mesage body";
+
+        ///////////////////////////////////////////////////////////////////
+        // this sends the email
+        $success = mail($toEmail, $subject, $message, $headers);
+        if (!$success) {
+            echo "<p>email NOT sent!</p>";
+        } else {
+            echo "email was sent";
+        }
+        ////////////////////////////////////////////////////////////////
+        ?>
+        <!-- <h1>Form submission was successful</h1> -->
+
+    </body>
+</html>
