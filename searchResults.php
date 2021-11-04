ffe11af52c18fad7a638206e33090e9147cca3da
Conor O'Brien <91850829+conorep@users.noreply.github.com>
2021-11-04 08:46:02 -0700

    Add files via upload
    changed order of stylesheet links

--- searchResults.php
+++ searchResults.php
@@ -21,11 +21,12 @@
     <!--    Bootstrap Styles and Main Styles-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/styles.css">
-    <link rel="stylesheet" href="styles/headerWidth_responsive.css">
+
 
     <!-- Kevin's CSS, Next Two Lines -->
     <link rel="stylesheet" href="styles/questionButtonAndForm_styles.css">
     <link rel="stylesheet" href="styles/questionButtonAndForm_responsiveStyles.css">
+    <link rel="stylesheet" href="styles/headerWidth_responsive.css">
 
     <!--  Favicon  -->
     <link rel="icon" type="img/jpg" href="images/img.png" >
