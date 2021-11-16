5971c49b4c2e4755567927822f9bf86f47f60b3d
conor <Obrien.Conor@student.greenriver.edu>
2021-11-15 19:01:48 -0800

    added a toast to the admin logged-in page, added styling to handle it.

--- admin/adminPanel.php
+++ admin/adminPanel.php
@@ -104,6 +104,12 @@
 
 <div class="container py-4"><!--content container-->
 
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="1500" >
+        <div class="toast-body">
+            <strong>Admin login successful!</strong>
+        </div>
+    </div>
+
     <table id="guestbook-entries" class="display " style="width:100%">
         <thead>
         <tr>
@@ -131,8 +137,6 @@
             // display client questions
             $sql ="SELECT * FROM client_questions";
             $result = mysqli_query($cnxn, $sql);
-
-            /*echo "<p>$result</p>";*/
 
             foreach($result as $row) {
 
@@ -263,15 +267,14 @@
 </footer>
 <!--End footer here-->
 
-<!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
 <!--is this needed-->
@@ -289,6 +292,10 @@
             responsive: true
         }
     );
+
+    $(document).ready(function() {
+        $('.toast').toast('show');
+    });
 </script>
 
 
