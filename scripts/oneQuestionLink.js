6d28da31a51b2d7c114c4b22a71abd0b0aa2d7c8
McZazz <91503026+McZazz@users.noreply.github.com>
2021-11-28 21:32:34 -0800

    .

--- scripts/oneQuestionLink.js
+++ scripts/oneQuestionLink.js
@@ -8,7 +8,7 @@ for (let i = 0; i < accordion_items.length; i++) {
         // get the id
         let the_id = event.target.id;
         the_id = the_id.slice(18, the_id.length)
-        console.log(the_id)
+        // console.log(the_id)
         // copy link to clipboard
         navigator.clipboard.writeText('https://' + location.hostname + '/question.php?id=' + the_id); //////////////////////////// SWAP
         // navigator.clipboard.writeText('localhost:8000/question.php?id=' + the_id);
