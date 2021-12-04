b7844aa48b9282a5be308ecec5bd9287e8b37ff7
conor <Obrien.Conor@student.greenriver.edu>
2021-12-03 19:45:11 -0800

    altered the anchor to go to new question and category page to also include new admin email change function

--- admin/adminPanel.php
+++ admin/adminPanel.php
@@ -118,7 +118,7 @@ $adminFooter = 'yes';
 
 <!-- content container -->
 <div class='container py-4'>
-    <p class="mb-3"><a href="adminAddQuestion.php">New Question, Category, Admin Email</a></p>
+    <p class="mb-3"><a href="adminAddQuestion.php">NEW QUESTION, CATEGORY, ADMIN EMAIL</a></p>
 
     <div class='toast' role='alert' aria-live='assertive' aria-atomic='true' data-bs-delay='1500'>
         <div class='toast-body'>
